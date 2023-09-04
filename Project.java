3a81e02cc9d688281947bccbbbe0f49c09fd2f64
ArtiomVert <asayarov@gmail.com>
2023-09-04 19:07:57 +0300

    Add file with code

---
+++ Project.java
@@ -0,0 +1,11 @@
+class Project {
+	public static void main(String args[]){
+
+		 for (int i = 0; i<50;i ++){
+		 	int s = 1;
+		 	for (int q = 0; q<6;q ++){s = s*i;}
+		 	System.out.println(s);
+		 }
+	}
+	
+}
