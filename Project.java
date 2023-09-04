999a6537c5eaf407c8f209f46c17c31a6eb946cc
ArtiomVert <asayarov@gmail.com>
2023-09-04 20:06:54 +0300

    Added void get_number_in_words()

--- Project.java
+++ Project.java
@@ -1,13 +1,21 @@
 class Project {
 
-	String [] list = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	public static String [] list = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-	public String get_number_in_words(int num){
-		return list[0];
+	public static String get_number_in_words(int num){
+		String s = Integer.toString(num);
+		String s_out = new String("");
+		int len_s = s.length();
+		for (int i = 0; i<len_s-1; i++){
+			s_out = s_out + list[Integer.valueOf(s.substring(0, 1))] + " ";
+			s = s.substring(1);
+		}
+		s_out = s_out + list[Integer.valueOf(s)];
+		return s_out;
 	}
 
 	static void pows(){
-		for (int i = 0; i<50;i ++){
+		for (int i = 1; i<51;i ++){
 		 	int s = 1;
 		 	for (int q = 0; q<5;q ++){s = s*i;}
 		 	System.out.println(s);
@@ -15,8 +23,7 @@ class Project {
 	}
 
 	public static void main(String args[]){
-		pows();
-		 
+		 System.out.println(get_number_in_words(12300));
 	}
 	
 }
