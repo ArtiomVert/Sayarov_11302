ead547a4262f92651f374dc9949b37b7a79abb0c
ArtiomVert <asayarov@gmail.com>
2023-09-04 19:29:35 +0300

    Added void pows()

--- Project.java
+++ Project.java
@@ -1,11 +1,22 @@
 class Project {
-	public static void main(String args[]){
 
-		 for (int i = 0; i<50;i ++){
+	String [] list = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+	public String get_number_in_words(int num){
+		return list[0];
+	}
+
+	static void pows(){
+		for (int i = 0; i<50;i ++){
 		 	int s = 1;
-		 	for (int q = 0; q<6;q ++){s = s*i;}
+		 	for (int q = 0; q<5;q ++){s = s*i;}
 		 	System.out.println(s);
-		 }
+		}
+	}
+
+	public static void main(String args[]){
+		pows();
+		 
 	}
 	
 }
