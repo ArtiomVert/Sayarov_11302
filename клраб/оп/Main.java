1e524f475753d19465ef3d219f574701562d198b
ArtiomVert Acer <asayarov@gmail.com>
2023-12-06 17:13:34 +0300

    Add throw ZeroException in RationalFraction

--- клраб/оп/Main.java
+++ клраб/оп/Main.java
@@ -3,20 +3,11 @@ public class Main{
 		System.out.println(o);
 	}
 	public static void main(String[] args) {
-		RationalFraction r1 = new RationalFraction(1, 1);
-		RationalFraction r2 = new RationalFraction(1, 2);
-		RationalComplexNumber q1 = new RationalComplexNumber(r1, r2);
-		RationalFraction r3 = new RationalFraction(1, 3);
-		RationalFraction r4 = new RationalFraction(1, 4);
-		RationalComplexNumber q2 = new RationalComplexNumber(r3, r4);
-		RationalFraction r5 = new RationalFraction(1, 5);
-		RationalFraction r6 = new RationalFraction(1, 6);
-		RationalComplexNumber q3 = new RationalComplexNumber(r5, r6);
-		RationalFraction r7 = new RationalFraction(1, 7);
-		RationalFraction r8 = new RationalFraction(1, 8);
-		RationalComplexNumber q4 = new RationalComplexNumber(r7, r8);
-		RationalComplexMatrix2x2 m1 = new RationalComplexMatrix2x2(q1, q2, q3, q4);
-		m1.add(m1);
-		m1.mult(m1);
+		try{
+			RationalFraction r1 = new RationalFraction(1, 0);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
 }
--- клраб/оп/RationalFraction.java
+++ клраб/оп/RationalFraction.java
@@ -2,13 +2,16 @@ public class RationalFraction{
 	private int c;
 	private int z;
 	public boolean isPos(){return c>=0;}
-	public RationalFraction(){
+	public RationalFraction()throws ZeroExeption{
 		set(0, 1);
 	}
-	public RationalFraction(int c, int z){
+	public RationalFraction(int c, int z)throws ZeroExeption{
 		set(c, z);
 	}
-	private void set(int c, int z){
+	private void set(int c, int z) throws ZeroExeption{
+		if (z==0){
+			throw new ZeroExeption();
+		}
 		this.c = c*z/abs(z);
 		this.z = abs(z);
 		reduce();
@@ -32,28 +35,28 @@ public class RationalFraction{
 	private int sum(RationalFraction r, int a){
 		return c * r.z + a * r.c * z;
 	}
-	public RationalFraction add(RationalFraction r){
+	public RationalFraction add(RationalFraction r)throws ZeroExeption{
 		return new RationalFraction(sum(r, 1), z * r.z);
 	}
-	public void add2(RationalFraction r){
+	public void add2(RationalFraction r)throws ZeroExeption{
 		set(sum(r, 1), z * r.z);
 	}
-	public RationalFraction sub(RationalFraction r){
+	public RationalFraction sub(RationalFraction r)throws ZeroExeption{
 		return new RationalFraction(sum(r, -1), z * r.z);
 	}
-	public void sub2(RationalFraction r){
+	public void sub2(RationalFraction r)throws ZeroExeption{
 		set(sum(r, -1), z * r.z);
 	}
-	public RationalFraction mult(RationalFraction r){
+	public RationalFraction mult(RationalFraction r)throws ZeroExeption{
 		return new RationalFraction(c*r.c, z*r.z);
 	}
-	public void mult2(RationalFraction r){
+	public void mult2(RationalFraction r)throws ZeroExeption{
 		set(c*r.c, z*r.z);
 	}
-	public RationalFraction div(RationalFraction r){
+	public RationalFraction div(RationalFraction r)throws ZeroExeption{
 		return new RationalFraction(c*r.z, z*r.c);
 	}
-	public void div2(RationalFraction r){
+	public void div2(RationalFraction r)throws ZeroExeption{
 		set(c*r.z, z*r.c);
 	}
 	public double value(){
@@ -61,5 +64,10 @@ public class RationalFraction{
 	}
 	public boolean equals(RationalFraction r){
 		return c==r.c && z==r.z;
+	}
+}
+class ZeroExeption extends Exception{
+	public ZeroExeption(){
+		super("The denominator can't be 0");
 	}
 }
