ac896d2fd9d138edcc32a58367308cf75d98cdca
ArtiomVert <asayarov@gmail.com>
2024-04-28 01:41:15 +0300

    Update convert utf8 to w-1251

--- work/src/homeworks/DZ4/Task26.java
+++ work/src/homeworks/DZ4/Task26.java
@@ -1,22 +1,43 @@
 package homeworks.DZ4;
 
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Task26 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         FileInputStream in = new FileInputStream("work\\res\\task_for_dz4_26.txt");
         FileOutputStream out = new FileOutputStream("work\\res\\task_for_dz4_26_out.txt");
-        byte[] bytes = new byte[110];
-        in.read(bytes);
-        String s_utf8 = new String(bytes, "UTF-8");
-        String s_w1251 = new String(bytes, "windows-1251");
-        System.out.println(s_w1251);
-        System.out.println(s_utf8);
-        out.write(s_w1251.getBytes());
+        ArrayList<Byte> bs = new ArrayList<>();
+        int data = in.read();
+        while (data != -1) {
+            if (data == 208) {
+                int data2 = in.read();
+                if (data2 == 129) {//Ё
+                    out.write(168);
+                } else if (data2 <= 191 && data2 >= 144) {//А-Я, а-п
+                    out.write((data2 + 48));
+                } else {
+                    out.write(data);
+                    out.write(data2);
+                }
+            } else if (data == 209) {
+                int data2 = in.read();
+                if (data2 == 145) {//ё
+                    out.write(184);
+                } else if (data2 <= 143 && data2 >= 128) {//р-я
+                    out.write((data2 + 112));
+                } else {
+                    out.write(data);
+                    out.write(data2);
+                }
+            } else {
+                out.write(data);
+            }
+            data = in.read();
+        }
+        in.close();
+        out.close();
     }
 }
