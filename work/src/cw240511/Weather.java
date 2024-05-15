31c1e156d30e1dbf41986688e089640e64b0b3b7
ArtiomVert <asayarov@gmail.com>
2024-05-16 01:54:49 +0300

    Weather completed

--- work/src/cw240511/Weather.java
+++ work/src/cw240511/Weather.java
@@ -2,6 +2,8 @@ package cw240511;
 
 import cw240511.XML.*;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 
@@ -22,29 +24,50 @@ public class Weather {
             data = is.read();
         }
         XML x = Parser.getXML(s);
-
-
         Teg start = x.tegs.get(0);
-        for (Teg t:start.tegs) {
+        try (FileOutputStream out = new FileOutputStream("work\\src\\cw240511\\file.csv", true)) {
+            out.write("city,temperature,date\n".getBytes());
+            Teg city = find(start, "city");
+            Teg temperature = find(start, "temperature");
+            Teg date = find(start, "lastupdate");
+            String out_string = city.atr.get("name") + "," + temperature.atr.get("value") + "," + date.atr.get("value")+"\n";
+            out.write(out_string.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (Teg t : start.tegs) {
             print(t, 0);
         }
     }
-    static String q(int c){
+
+    static String q(int c) {
         String s = "";
         for (int i = 0; i < c; i++) {
-            s+="   ";
+            s += "   ";
         }
         return s;
     }
-    static void print(Teg t, int space){
+
+    static void print(Teg t, int space) {
         String sp = q(space);
-        System.out.print(sp+t.name+": ");
-        for (String a: t.atr.keySet()){
-            System.out.print(a+"="+t.atr.get(a)+" ");
+        System.out.print(sp + t.name + ": ");
+        for (String a : t.atr.keySet()) {
+            System.out.print(a + "=" + t.atr.get(a) + " ");
         }
         System.out.println();
-        for (Teg t1:t.tegs){
-            print(t1, space+1);
+        for (Teg t1 : t.tegs) {
+            print(t1, space + 1);
         }
+    }
+
+    static Teg find(Teg start, String target_name_teg) {
+        if (start.name.equals(target_name_teg)) {
+            return start;
+        }
+        for (Teg t : start.tegs) {
+            Teg f = find(t, target_name_teg);
+            if (f != null) return f;
+        }
+        return null;
     }
 }
