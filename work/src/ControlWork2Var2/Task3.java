2df8e089af59d248becf9e1a65f4ae1858717154
ArtiomVert Acer <asayarov@gmail.com>
2024-06-11 17:05:05 +0300

    Less Memory

--- work/src/ControlWork2Var2/Task3.java
+++ work/src/ControlWork2Var2/Task3.java
@@ -2,34 +2,59 @@ package ControlWork2Var2;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Task3 {
-    public static void main(String[] args) {
+    static void write() {
+        Map<String, Integer> races = new HashMap<>();
+        Reader.getStream().forEach(h -> races.put(h.race, null));
         try (FileOutputStream out = new FileOutputStream("work\\src\\ControlWork2Var2\\DataSet")) {
+            out.write(races.size());
+            int i = 0;
+            for (Map.Entry<String, Integer> s : races.entrySet()) {
+                out.write(s.getKey().getBytes().length);
+                out.write(s.getKey().getBytes());
+                s.setValue(i);
+                i++;
+            }
             Reader.getStream().forEach((h -> {
                 try {
-                    out.write((byte) h.race.getBytes().length);
-                    out.write(h.race.getBytes());
-                    out.write((byte) h.education);
+                    out.write(races.get(h.race));
+                    out.write(h.education);
                 } catch (Exception e) {
                 }
             }));
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    static void read() {
         try (FileInputStream is = new FileInputStream("work\\src\\ControlWork2Var2\\DataSet")) {
-            int length = is.read();
-            while (length != -1) {
-                byte[] bs = new byte[length];
-                for (int i = 0; i < length; i++) {
-                    bs[i] = (byte) is.read();
-                }
-                String race = new String(bs);
-                System.out.println(race + " " + is.read());
-                length = is.read();
+            int count_races = is.read();
+            String[] races = new String[count_races];
+            for (int i = 0; i < count_races; i++) {
+                int length = is.read();
+                byte[] race_in_byte = new byte[length];
+                is.read(race_in_byte);
+                races[i] = new String(race_in_byte);
+            }
+            int index_race = is.read();
+            while (index_race != -1) {
+                int education = is.read();
+                System.out.println(races[index_race] + " " + education);
+                index_race = is.read();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        write();
+        read();
     }
 }
